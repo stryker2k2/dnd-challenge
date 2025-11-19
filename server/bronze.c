aae9cb55560e4b12418cf313aaa16aa78b261327
Jack Ward <stryker2k2@msn.com>
2025-11-19 21:11:40 +0000

    Modified Bronze Challenge to have CTF Flag

--- server/bronze.c
+++ server/bronze.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    char str[6] = "hello";
-    char overflow[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    strcpy(str, overflow);
+int logToFile(char *logTxt)
+{ 
+    char tmpLog[64];
+    printf("[+] logging to file\n");
+    sprintf(tmpLog, "[+] %s", logTxt);
+    FILE *logFile = fopen("log.txt", "w");
+    if (logFile == NULL)
+    {
+        return 1;
+    }
 
-    printf("str: \"%s\"", str);
+    fprintf(logFile, "%s\n", tmpLog);
+    fclose(logFile);
 
     return 0;
 }
+
+int main(int argc, char *argv[])
+{
+    logToFile(argv[1]);
+}
+
+
+
+// How to Compile:
+// gcc -m32 -no-pie -O0 -Wno-format-truncation -fno-stack-protector bronze.c -o bronze
